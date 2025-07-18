d65e2f32ffbe5bb62d4c95647e4aec3ba622a889
Phoo Wai Ko <phoowaiko0@gmail.com>
2025-07-18 15:40:33 +0630

    Fixed

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,8 +2,8 @@ import { stripe } from "@/lib/stripe";
 import { ProductDetail } from "@/components/product-detail";
 
 const Productpage = async ({ params }: { params: Promise<{ id: string }> }) => {
-  const productId = (await params).id;
-  const product = await stripe.products.retrieve(productId, {
+  const { id } = await params;
+  const product = await stripe.products.retrieve(id, {
     expand: ["default_price"],
   });
   const plainProduct = JSON.parse(JSON.stringify(product));
